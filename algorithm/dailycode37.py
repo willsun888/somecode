32f012af2b991776e2da1c879a342cdb9bdb3445
willqqsun <willqqsun@tencent.com>
2019-06-03 23:28:57 +0800

    modify test

--- algorithm/dailycode37.py
+++ algorithm/dailycode37.py
@@ -4,7 +4,7 @@ def get_subset(lst):
     if len(lst) == 0:
         return [[]]
     sset = get_subset(lst[1:])
-    return sset + [ e+[lst[0]] for e in sset ]
+    return sset + [ s+[lst[0]] for s in sset ]
 
 if __name__ == "__main__":
     lst = [1, 2, 3, 4]
